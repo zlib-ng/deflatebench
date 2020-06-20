c181d8f9b6dcc363aed17ec1e6846d188264833b
Hans Kristian Rosbach <hk-git@circlestorm.org>
2020-06-20 18:20:12 +0200

    Implement support for 3 different modes: - Single file testing for all levels - Multi file testing based on a list with different file per level - Multi file testing based on concating a source file multiple times to generate files of a certain size per level
    Also various smaller improvements to make this tool easier to use for others.

--- deflatebench.py
+++ deflatebench.py
@@ -2,6 +2,7 @@
 import os, os.path
 import sys
 import re
+import math
 import shlex
 import shutil
 import hashlib
@@ -9,32 +10,68 @@ import subprocess
 from subprocess import PIPE
 from datetime import datetime, date, time, timedelta
 
-# Define some variables
+## Define some variables
 runs = 15
 trimcount = 5
-minlevel = 1
+minlevel = 0
 maxlevel = 9
 
 start_delay = 0  # Milliseconds of startup to skip measuring, requires usleep(X*1000) in minigzip main()
+
+skipverify = False
+skipdecomp = False
+skipworst = True
+
+temp_path = "/tmp/"
+
+## CPU and measurement related settings
 use_chrt = True
 use_nosync = True
 use_perf = True
 use_turboctl = True
 use_cpupower = True
 
-skipverify = False
-skipdecomp = False
-skipworst = True
+cpu_minspeed = 1000
+cpu_maxspeed = 3600
 
-#testfile = "../testfil-500M"
-#testfile = "../testfil-100M"
-testfile = "../testfil-40M"
-#testfile = "../testfil-30M"
-#testfile = "../testfil-20M"
+## Testfile definition
+use_single_testfile = False
+use_generated_testfiles = True  # Only valid if use_single_testfile == False
 
-tempfile = "/tmp/runtest2.tmp"
+# Single testfile
+single_testfile = '../silesia.tar'
 
+multi_testfiles = {
+    0: '../testfil-500M',
+    1: '../testfil-300M',
+    2: '../testfil-150M',
+    3: '../testfil-125M',
+    4: '../testfil-100M',
+    5: '../testfil-85M',
+    6: '../testfil-75M',
+    7: '../testfil-40M',
+    8: '../testfil-20M',
+    9: '../testfil-20M'
+}
 
+# Generated testfiles
+gen_testfilesrc = '../testfil-15M'
+gen_testsizes = {
+    0: 500,
+    1: 300,
+    2: 150,
+    3: 125,
+    4: 100,
+    5: 85,
+    6: 75,
+    7: 40,
+    8: 20,
+    9: 20,
+}
+
+BUF_SIZE = 1024*1024  # lets read stuff in 1MB chunks when hashing or copying
+
+# ANSI color codes
 BLUE = '\033[34m'
 GREEN = '\033[32m'
 RED = '\033[31m'
@@ -56,7 +93,7 @@ def cputweak(enable):
         if use_turboctl:
             runcommand('sudo /usr/bin/turboctl off', silent=1)
         if use_cpupower:
-            runcommand('sudo /usr/bin/cpupower frequency-set -g performance -d 3600000', silent=1)
+            runcommand(f'sudo /usr/bin/cpupower frequency-set -g performance -d {cpu_maxspeed*1000}', silent=1)
             runcommand('sudo /usr/bin/cpupower idle-set -D 2', silent=1)
 
     # Turn cpu turbo and power savings back on
@@ -64,7 +101,7 @@ def cputweak(enable):
         if use_turboctl:
             runcommand('sudo /usr/bin/turboctl on')
         if use_cpupower:
-            runcommand('sudo /usr/bin/cpupower frequency-set -d 1000000')
+            runcommand(f'sudo /usr/bin/cpupower frequency-set -d {cpu_minspeed*1000}')
             runcommand('sudo /usr/bin/cpupower idle-set -E')
 
 def get_len(s):
@@ -78,7 +115,6 @@ def printnn(text):
 
 def hashfile(file):
     ''' Calculate hash of file '''
-    BUF_SIZE = 1024*1024  # lets read stuff in 1MB chunks!
     sha1 = hashlib.sha1()
 
     with open(file, 'rb') as f:
@@ -88,6 +124,22 @@ def hashfile(file):
                 break
             sha1.update(data)
     return sha1.hexdigest()
+
+def generate_testfile(sourcefile,destfile,minsize):
+    ''' Make tempfiles that are concatenated repeatedly until the file is big enough '''
+    srcsize = os.path.getsize(sourcefile)
+    count = math.ceil((minsize*1024*1024)/srcsize)
+    dstsize = srcsize*count
+
+    dst = open(destfile, "wb")
+    with open(sourcefile, 'rb') as src:
+        while os.path.getsize(destfile) < dstsize:
+            data = src.read(BUF_SIZE)
+            if not data:
+                src.seek(0)
+                continue
+            dst.write(data)
+    dst.close()
 
 def runcommand(command, env=None, stoponfail=1, silent=1, output='/dev/null'):
     ''' Run command, and handle special cases '''
@@ -131,24 +183,27 @@ def parse_timefile(filen):
             content = f.readlines()
         for line in content:
             if line[-13:-1] == 'seconds user':
-                return(float(line[:-13]))
+                return float(line[:-13])
         return 0.0
     else:
         with open(filen) as f:
             content = f.readlines()
-        return(float(content[0]))
+        return float(content[0])
 
 def resultstr(min, avg, max):
     ''' Build result string '''
     return f"{BLUE}{min:.3f}{RESET}/{GREEN}{avg:.3f}{RESET}/{RED}{max:.3f}{RESET}"
 
-def runtest(file,orighash,level,skipverify,skipdecomp):
+def runtest(tempfiles,level,skipverify,skipdecomp):
     ''' Run the tests for a compression level'''
     timefile = '/tmp/zlib-time.tmp'
     compfile = '/tmp/zlib-testfil.gz'
     decompfile = '/tmp/zlib-testfil.raw'
     hashfail = 0
     decomptime = 0
+
+    file = tempfiles[level]['filename']
+    orighash = tempfiles[level]['hash']
 
     env = get_env(True)
     cmdprefix = command_prefix(timefile)
@@ -181,7 +236,7 @@ def runtest(file,orighash,level,skipverify,skipdecomp):
     # Validate using gunzip
     if not skipverify:
         printnn('v')
-        runcommand("gunzip -k -c %s" % (compfile), output=decompfile)
+        runcommand("gunzip -c %s" % (compfile), output=decompfile)
 
         gziphash = hashfile(decompfile)
         if gziphash != orighash:
@@ -203,14 +258,13 @@ def trimworst(results):
     results.sort()
     return results[:-trimcount]
 
-def printreport(results, origsize):
+def printreport(results, tempfiles):
     ''' Print results table '''
-    totsize = 0
-    totcomptime = 0
-    totdecomptime = 0
-    totsize2 = 0
-    totcomptime2 = 0
-    totdecomptime2 = 0
+    totsize, totsize2 = [0]*2
+    totorigsize, totorigsize2 = [0]*2
+    totcomppct, totcomppct2 = [0]*2
+    totcomptime, totcomptime2 = [0]*2
+    totdecomptime, totdecomptime2 = [0]*2
     numresults = runs - trimcount
 
     numlevels = len(range(minlevel,maxlevel+1))
@@ -220,19 +274,20 @@ def printreport(results, origsize):
     print(" Skipworst: %s (%s)" % (skipworst, trimcount))
 
     if skipdecomp:
-        print("\n Level   Comp   Comptime min/avg/max")
+        print("\n Level   Comp   Comptime min/avg/max                          Compressed size")
     else:
-        print("\n Level   Comp   Comptime min/avg/max  Decomptime min/avg/max")
+        print("\n Level   Comp   Comptime min/avg/max  Decomptime min/avg/max  Compressed size")
 
-    for i in range(0,numlevels):
+    for level in range(0,numlevels):
         ltotcomptime, ltotdecomptime = [0,0]
+        origsize = tempfiles[level]['origsize']
 
         # Find best/worst times for level
         compsize = None
         rawcomptimes = []
         rawdecomptimes = []
         for run in results:
-            rlevel,rsize,rcompt,rdecompt = run[i]
+            rlevel,rsize,rcompt,rdecompt = run[level]
             rawcomptimes.append(rcompt)
             rawdecomptimes.append(rdecompt)
             if not compsize is None and compsize != rsize:
@@ -254,19 +309,23 @@ def printreport(results, origsize):
         ltotcomptime += sum(comptimes)
         ltotdecomptime += sum(decomptimes)
 
-        # Store values for grand total
-        totsize += rsize
-        totcomptime += ltotcomptime
-        totdecomptime += ltotdecomptime
-        if i != 0:
-            totsize2 += rsize
-            totcomptime2 += ltotcomptime
-            totdecomptime2 += ltotdecomptime
-
         # Compute and print values for this level
-        comp = float(rsize*100)/origsize
+        comppct = float(rsize*100)/origsize
         avgcomptime = ltotcomptime/numresults
         avgdecomptime = ltotdecomptime/numresults
+
+        # Store values for grand total
+        totsize += rsize
+        totorigsize += origsize
+        totcomppct += comppct
+        totcomptime += ltotcomptime
+        totdecomptime += ltotdecomptime
+        if level != 0:
+            totsize2 += rsize
+            totorigsize2 += origsize
+            totcomppct2 += comppct
+            totcomptime2 += ltotcomptime
+            totdecomptime2 += ltotdecomptime
 
         # Print level results
         compstr = resultstr(mincomptime, avgcomptime, maxcomptime)
@@ -277,16 +336,16 @@ def printreport(results, origsize):
         compstrpad = ' ' * (20 - get_len(compstr))
         decompstrpad = ' ' * (23 - get_len(decompstr))
 
-        print(" %-5s %7.3f%% %s%s %s%s  %s " % (rlevel,comp,compstrpad,compstr,decompstrpad,decompstr,compsize))
+        print(" %-5s %7.3f%% %s%s %s%s  %s " % (rlevel,comppct,compstrpad,compstr,decompstrpad,decompstr,compsize))
 
     ### Totals
-    # Compression times
-    avgcomp = (float(totsize/numlevels)*100)/origsize
-    avgcomp2 = (float(totsize2/(numlevels-1))*100)/origsize
+    # Compression
+    avgcomppct = totcomppct/numlevels
+    avgcomppct2 = totcomppct2/(numlevels-1)
     avgcomptime = totcomptime/(numlevels*numresults)
     avgcomptime2 = totcomptime2/((numlevels-1)*numresults)
 
-    # Decompression times
+    # Decompression
     if skipdecomp:
         avgdecomptime, avgdecompstr, totdecompstr = [''] * 3
         avgdecomptime2, avgdecompstr2, totdecompstr2 = [''] * 3
@@ -299,18 +358,14 @@ def printreport(results, origsize):
             avgdecompstr2 = "%.3f" % (avgdecomptime2)
             totdecompstr2 = "%.3f" % (totdecomptime2)
 
-    print("\n %-5s %7.3f%% %20.3f %23s" % ('avg1',avgcomp,avgcomptime,avgdecompstr))
+    print("\n %-5s %7.3f%% %20.3f %23s" % ('avg1',avgcomppct,avgcomptime,avgdecompstr))
     if minlevel == 0:
-        print(" %-5s %7.3f%% %20.3f %23s  (lvl 0 excluded)" % ('avg2',avgcomp2,avgcomptime2,avgdecompstr2))
+        print(" %-5s %7.3f%% %20.3f %23s  (lvl 0 excluded)" % ('avg2',avgcomppct2,avgcomptime2,avgdecompstr2))
     print(" %-5s %8s %20.3f %23s" % ('tot','',totcomptime,totdecompstr))
 
 
 def main():
     global skipworst, skipverify, runs, trimcount
-    shutil.copyfile(testfile,tempfile)
-    hash = hashfile(tempfile)
-    origsize = os.path.getsize(tempfile)
-
     if len(sys.argv) >= 2:
         runs = int(sys.argv[1])
 
@@ -321,6 +376,43 @@ def main():
     if skipworst and runs <= trimcount:
         timcount = runs - 1
 
+    # Prepare tempfiles
+    tempfiles = dict()
+
+    # Single testfile, we just reference the same file for every level
+    if use_single_testfile:
+        tmp_filename = os.path.join(temp_path, f"deflatebench.tmp")
+        shutil.copyfile(single_testfile,tmp_filename)
+        hash = hashfile(tmp_filename)
+        origsize = os.path.getsize(tmp_filename)
+        print(f"Activated single file mode: {single_testfile} {origsize/1024/1024}MiB")
+
+        for level in range(minlevel,maxlevel+1):
+            tempfiles[level] = dict()
+            tempfiles[level]['filename'] = tmp_filename
+            tempfiles[level]['hash'] = hash
+            tempfiles[level]['origsize'] = origsize
+    else:
+        # Multiple testfiles
+        if use_generated_testfiles:
+            print(f"Activated multiple generated file mode. Source: {gen_testfilesrc}")
+        else:
+            print(f"Activated multiple file mode.")
+
+        for level in range(minlevel,maxlevel+1):
+            tempfiles[level] = dict()
+            tmp_filename = os.path.join(temp_path, f"deflatebench-{level}.tmp")
+            tempfiles[level]['filename'] = tmp_filename
+
+            if use_generated_testfiles:
+                generate_testfile(gen_testfilesrc,tmp_filename,gen_testsizes[level])
+            else:
+                shutil.copyfile(multi_testfiles[level],tmp_filename)
+
+            tempfiles[level]['hash'] = hashfile(tmp_filename)
+            tempfiles[level]['origsize'] = os.path.getsize(tmp_filename)
+
+    # Tweak system to reduce benchmark variance
     cputweak(True)
 
     # Run tests and record results
@@ -332,17 +424,19 @@ def main():
         temp = []
         print("Starting run %s of %s" % (run,runs))
         for level in range(minlevel,maxlevel+1):
-            compsize,comptime,decomptime,hashfail = runtest(tempfile,hash,level,skipverify,skipdecomp)
+            compsize,comptime,decomptime,hashfail = runtest(tempfiles,level,skipverify,skipdecomp)
             if hashfail != 0:
                 print("ERROR: level %s failed crc checking" % (level))
             temp.append( [level,compsize,comptime,decomptime] )
         results.append(temp)
 
-    printreport(results, origsize)
+    printreport(results, tempfiles)
 
+    # Disable system tweaks to restore normal powersaving, turbo, etc
     cputweak(False)
 
-    os.unlink(tempfile)
-
+    # Clean up tempfiles
+    for level in range(minlevel,maxlevel+1):
+        os.unlink(tempfiles[level]['filename'])
 
 main()
